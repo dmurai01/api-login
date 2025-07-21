62c9a01f2f21583f1250bbb78e761731128c388e
Daniela Murai <dany.murai@gmail.com>
2025-07-21 10:00:38 -0300

    iniciando testes para login post

--- tests/cadastroUsuario.test.js
+++ tests/cadastroUsuario.test.js
@@ -10,22 +10,20 @@ describe('GET /api/users/register', () => {
     it('Deve retornar sucesso 201 quando cadastrar um novo usuário com dados válidos', async () => {
         const statusCadastro = await cadastrarNovoUsuario(nome, email, '123456')
         expect(statusCadastro).to.equal(201)
-    });
+    })
 
     it('Deve retornar erro 400 quando cadastrar com dados faltando', async () => {
         nome = ''
 
         const statusCadastro = await cadastrarNovoUsuario(nome, email, '123456')
         expect(statusCadastro).to.equal(400)
-    });
+    })
 
     it('Deve retornar erro 409 quando cadastar com email já cadastrado', async () => {
         nome = 'Teste Email'
         
         const statusCadastro = await cadastrarNovoUsuario(nome, email, '123456')
         expect(statusCadastro).to.equal(409)
-    });
+    })
 
-
-
-});
+})
--- tests/login.test.js
+++ tests/login.test.js
@@ -1,3 +1,15 @@
 const request = require('supertest')
 const { expect } = require('chai')
-require('dotenv').config
+require('dotenv').config()
+const postLogin = require('../fixtures/postLogin.json')
+
+describe('Login', () => {
+    describe('POST /api/users/login', () => {
+        it('Deve retornar sucesso 200 com token quando usar credenciais válidas de usuário não bloqueado', () => {
+            
+        });
+        
+    });
+    
+});
+
