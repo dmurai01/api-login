b612fbe7afe8df7f650c91bf6e2cfc8cf3627c44
Daniela Murai <dany.murai@gmail.com>
2025-07-29 12:19:54 -0300

    incluindo configuração cors para permitir requisições de dominios diferentes

--- src/app.js
+++ src/app.js
@@ -3,8 +3,17 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger');
+const cors = require('cors');
 
 const app = express();
+
+// Permitir requisições de qualquer origem (ou especifique a origem do seu frontend)
+app.use(cors({
+  // origin: 'http://192.168.1.103:4000', // ou use '*' para todas as origens (não recomendado para produção)
+  origin: '*',
+  credentials: true // se precisar enviar cookies/autenticação
+}));
+
 app.use(bodyParser.json());
 
 app.use('/api/users', userRoutes);
@@ -13,4 +22,5 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
-}); 
+});
+
